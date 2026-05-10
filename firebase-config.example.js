1ad5c8bb76718cf7a68d0f9227a4c3bc332ea7d7
kenta-horikoshi <christianwolff365@gmail.com>
2026-05-10 22:02:12 +0900

    Update firebase-config.example.js

--- firebase-config.example.js
+++ firebase-config.example.js
@@ -1,8 +1,8 @@
 export const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_PROJECT_ID.firebasestorage.app",
-  messagingSenderId: "YOUR_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyArmHXIp6g2dHmKZy0CxavceXVqR4XJp2A",
+  authDomain: "reji-1a767.firebaseapp.com",
+  projectId: "reji-1a767",
+  storageBucket: "reji-1a767.firebasestorage.app",
+  messagingSenderId: "605197140483",
+  appId: "1:605197140483:web:997f8ee71523997d2a0436",
 };
